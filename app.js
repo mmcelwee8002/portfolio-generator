56ee6e85ae82aec6efc2c3d91b84adab1adf92c6
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-01-23 19:50:39 -0600

    feature in progress

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+// const generatePage = () => 'Name: Jane, Github: janehub';
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
+
+console.log(name, github);
+console.log(generatePage(name, github));
 
 
 
-
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-  };
