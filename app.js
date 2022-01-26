5a31fc1291469ed7a9e7c5e529935057796c8fe9
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-01-25 18:41:50 -0600

    generate the HTML file

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
-// const generatePage = () => 'Name: Jane, Github: janehub';
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+const [name, github] = profileDataArgs;
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-console.log(name, github);
-console.log(generatePage(name, github));
-
-
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
